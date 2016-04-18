a2f2d8b42bd6021bf049a70db58c53cc1987a182
czlc <czlc0327@gmail.com>
2016-04-18 16:32:57 +0800

    some comment...

--- skynet-src/malloc_hook.h
+++ skynet-src/malloc_hook.h
@@ -1,10 +1,13 @@
+/* 线程安全的分配器 */
 #ifndef SKYNET_MALLOC_HOOK_H
 #define SKYNET_MALLOC_HOOK_H
 
 #include <stdlib.h>
 #include <lua.h>
 
+/* 获得总的内存分配量 */
 extern size_t malloc_used_memory(void);
+/* 获得总的内存块数 */
 extern size_t malloc_memory_block(void);
 extern void   memory_info_dump(void);
 extern size_t mallctl_int64(const char* name, size_t* newval);

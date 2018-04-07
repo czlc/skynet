8d66973bb2b9b57e3e11f2f7dc474d9225cb3ef8
czlc <czlc1983@gmail.com>
2018-04-07 11:57:14 +0800

    align

--- test/testdatasheet.lua
+++ test/testdatasheet.lua
@@ -25,24 +25,24 @@ if mode == "child" then
 
 else
 
-local builder = require "skynet.datasheet.builder"
-local datasheet = require "skynet.datasheet"
+	local builder = require "skynet.datasheet.builder"
+	local datasheet = require "skynet.datasheet"
 
-skynet.start(function()
-	builder.new("foobar", {a = 1, b = 2 , c = {3} })
-	skynet.newservice(SERVICE_NAME, "child")
-	local t = datasheet.query "foobar"
-	local c = t.c
-	dump(t, "[1]")
-	builder.update("foobar", { b = 4, c = { 5 } })
-	print("sleep")
-	skynet.sleep(100)
-	dump(t, "[2]")
-	dump(c, "[2.c]")
-	builder.update("foobar", { a = 6, c = 7, d = 8 })
-	print("sleep")
-	skynet.sleep(100)
-	dump(t, "[3]")
-end)
+	skynet.start(function()
+		builder.new("foobar", {a = 1, b = 2 , c = {3} })
+		skynet.newservice(SERVICE_NAME, "child")
+		local t = datasheet.query "foobar"
+		local c = t.c
+		dump(t, "[1]")
+		builder.update("foobar", { b = 4, c = { 5 } })
+		print("sleep")
+		skynet.sleep(100)
+		dump(t, "[2]")
+		dump(c, "[2.c]")
+		builder.update("foobar", { a = 6, c = 7, d = 8 })
+		print("sleep")
+		skynet.sleep(100)
+		dump(t, "[3]")
+	end)
 
 end
